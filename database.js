37ee12058c39b32c3ed62ab4b3024187c74b7951
Nbbyers15 <57930194+Nbbyers15@users.noreply.github.com>
2023-04-05 12:30:23 -0600

    added login functionality

---
+++ database.js
@@ -0,0 +1,74 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAMESIMON;
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
+const userCollection = client.db('startup').collection('user');
+const scoreCollection = client.db('startup').collection('drives');
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
+function addDrive(drive) {
+  scoreCollection.insertOne(drive);
+}
+
+function getDrives() {
+  const userName = localStorage.getItem('userName');
+  const query = {};
+  const options = {
+    sort: { score: -1 },
+    limit: 10,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
+}
+
+function getRides() {
+  const userName = localStorage.getItem('userName');
+  const query = {};
+  const options = {
+    sort: { score: -1 },
+    limit: 10,
+  };
+  const cursor = scoreCollection.find(query, options);
+  return cursor.toArray();
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  addDrive,
+  getDrives,
+  getRides,
+};
--- public/login.js
+++ public/login.js
@@ -1,16 +1,97 @@
-import {User} from './user.js';
+// import {User} from './user.js';
 
 
-let loginButton = document.getElementById('loginButton');
+// let loginButton = document.getElementById('loginButton');
 
-loginButton.addEventListener('click', () => {
-  console.log("here");
-  const userNameEl = document.querySelector("#username");
-  const user = new User(userNameEl.value);
-  localStorage.setItem("user", JSON.stringify(user));
-  //localStorage.setItem("userName", userNameEl.value);
-  console.log("Username: " + userNameEl.value);
-  window.location.href = "dashboard.html";
-})
+// loginButton.addEventListener('click', () => {
+//   console.log("here");
+//   const userNameEl = document.querySelector("#username");
+//   const user = new User(userNameEl.value);
+//   localStorage.setItem("user", JSON.stringify(user));
+//   //localStorage.setItem("userName", userNameEl.value);
+//   console.log("Username: " + userNameEl.value);
+//   window.location.href = "dashboard.html";
+// })
 
+
+
+
+
+(async () => {
+  let authenticated = false;
+  const userName = localStorage.getItem('userName');
+  if (userName) {
+    const nameEl = document.querySelector('#userName');
+    nameEl.value = userName;
+    const user = await getUser(nameEl.value);
+    authenticated = user?.authenticated;
+  }
+
+  if (authenticated) {
+    document.querySelector('#username').textContent = userName;
+    setDisplay('loginControls', 'none');
+    setDisplay('playControls', 'block');
+  } else {
+    setDisplay('loginControls', 'block');
+    setDisplay('playControls', 'none');
+  }
+})();
+
+async function loginUser() {
+  loginOrCreate(`/api/auth/login`);
+}
+
+async function createUser() {
+  loginOrCreate(`/api/auth/create`);
+}
+
+async function loginOrCreate(endpoint) {
+  const userName = document.querySelector('#userName')?.value;
+  const password = document.querySelector('#userPassword')?.value;
+  const response = await fetch(endpoint, {
+    method: 'post',
+    body: JSON.stringify({ email: userName, password: password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const body = await response.json();
+
+  if (response?.status === 200) {
+    localStorage.setItem('userName', userName);
+    window.location.href = 'dashboard.html';
+  } else {
+    const modalEl = document.querySelector('#msgModal');
+    modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+    const msgModal = new bootstrap.Modal(modalEl, {});
+    msgModal.show();
+  }
+}
+
+function play() {
+  window.location.href = 'dashboard.html';
+}
+
+function logout() {
+  fetch(`/api/auth/logout`, {
+    method: 'delete',
+  }).then(() => (window.location.href = '/'));
+}
+
+async function getUser(email) {
+  // See if we have a user with the given email.
+  const response = await fetch(`/api/user/${email}`);
+  if (response.status === 200) {
+    return response.json();
+  }
+
+  return null;
+}
+
+function setDisplay(controlId, display) {
+  const playControlEl = document.querySelector(`#${controlId}`);
+  if (playControlEl) {
+    playControlEl.style.display = display;
+  }
+}
 
